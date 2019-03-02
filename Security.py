e9f74d877abf1db7794ff520aa90609338d552d1
Mustafa Mossa <44642964+MMGit64@users.noreply.github.com>
2019-03-02 22:23:22 +0000

    Add files via upload

---
+++ Security.py
@@ -0,0 +1,40 @@
+def decrypt(Security):
+    
+    codeList = []
+    codeLength = len(Security)
+    codeRange = codeLength / 3
+
+    for i in range(0, codeRange):
+
+        x = i * 3
+        y = x + 3
+
+        codeRead = Security[x:y]
+
+        if codeRead == 'XXX' or codeRead == 'XYX':
+            codes = 'Intruder '
+        elif codeRead == 'XXY' or codeRead == 'XYY':
+            codes = 'Identified '
+        else:
+            codes = 'False Alarm '
+
+        codeList.append(codes)
+        codeJoin = "".join(codeList)
+    return codeJoin
+
+securityCode = raw_input("Enter your security code:")
+decryptedCode = decrypt(securityCode)
+print(decryptedCode)
+        
+        
+ofile = open("codeDecrypted.txt", "w")
+f = open("Securitycode.txt", "r+")
+for list in f:
+    #list = list.replace('X', 'Intruder') 
+    fileDecrypted = decrypt(list)
+    ofile.write(fileDecrypted)
+    #ofile.write(list)
+ofile.close()
+f.close()   
+
+            
